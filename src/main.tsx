c14b4fb407f12059a6106e594704153771db3160
Jello <jello.aka.liu@foxmail.com>
2023-12-24 15:43:57 +0800

    feat: 优化代码目录结构

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/global.scss';
-import HashRouterApp from "./App/HashRouterApp";
-import BrowserRouterApp from "./App/BrowserRouterApp";
+import HashRouterApp from "./app/HashRouterApp";
+import BrowserRouterApp from "./app/BrowserRouterApp";
 
 
 // ReactDOM.createRoot(document.getElementById('root')!).render(<HashRouterApp />);
